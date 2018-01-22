916d92db718c544cda81affec723bcf12373a4dc
Maria Furman <mafurman@microsoft.com>
2018-01-22 14:25:40 -0800

    Added RegexAnalyzer references to all projects

--- Tools/RegexAnalyzer/RegexAnalyzer/RegexAnalyzer.Test/RegexAnalyzerUnitTests.cs
+++ Tools/RegexAnalyzer/RegexAnalyzer/RegexAnalyzer.Test/RegexAnalyzerUnitTests.cs
@@ -1,16 +1,11 @@
-using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CodeFixes;
-using Microsoft.CodeAnalysis.Diagnostics;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
 using TestHelper;
-using RegexAnalyzer;
 
 namespace RegexAnalyzer.Test
 {
     [TestClass]
     public class UnitTest : CodeFixVerifier
     {
-
+        // TODO: Add in tests for the RegexAnalyzer.
     }   
 }
--- Tools/RegexAnalyzer/RegexAnalyzer/RegexAnalyzer/Resources.Designer.cs
+++ Tools/RegexAnalyzer/RegexAnalyzer/RegexAnalyzer/Resources.Designer.cs
@@ -8,11 +8,11 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace RegexAnalyzer {
-    using System;
+namespace RegexAnalyzer
+{
     using System.Reflection;
-    
-    
+
+
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
