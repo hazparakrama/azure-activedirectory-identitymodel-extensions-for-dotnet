5f4cb1d96dbe0d3e622b3a77b2e73a28a2bb37bb
Maria Furman <mafurman@microsoft.com>
2018-01-23 12:25:17 -0800

    Switching to dotnet restore instead of msbuild restore

--- Tools/RegexAnalyzer/build.ps1
+++ Tools/RegexAnalyzer/build.ps1
@@ -148,9 +148,11 @@ if ($build -eq "YES")
     }
     
     Write-Host ">>>" + $msbuildexe + "$root/RegexAnalyzer.sln  /t:restore"
-    & $msbuildexe "RegexAnalyzer.sln" "/t:restore"
+    #& $msbuildexe "RegexAnalyzer.sln" "/t:restore"
+	Start-Process -wait -NoNewWindow $dotnetexe "restore $root\RegexAnalyzer.sln"
     Write-Host ">>>" + $msbuildexe + "$root/RegexAnalyzer.sln  /t:build"
     & $msbuildexe "RegexAnalyzer.sln" "/t:build"
+	#Start-Process -wait -NoNewWindow $dotnetexe "build $root\RegexAnalyzer.sln"
 
     WriteSectionFooter("End Build");
 }
